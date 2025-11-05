d40639444fbc1ff332c111f9c15864d25183b988
AlejandroMGT52 <alejo.m15097@gmail.com>
2025-11-05 16:23:25 -0500

    modificaciones

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-# app.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '¡Hola desde mi aplicación Python en Docker y CI!'
+def hello():
+    return "¡Hola Mundo desde Flask con Traefik! 🚀"
 
 if __name__ == '__main__':
-    # Usamos 0.0.0.0 para que sea accesible dentro del contenedor Docker
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
